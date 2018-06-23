d39293c05e550f2d62e2cc0d7d443d9d9fe135c9
Carlota Pearl <carlota.pearl@gmail.com>
2018-06-23 19:58:08 -0400

    create card array that holds cards

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
+let cardArray = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-anchor', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-diamond', 'fa fa-bomb', 'fa fa-leaf', 'fa fa-bomb', 'fa fa-bolt', 'fa fa-bicycle', 'fa fa-paper-plane-o', 'fa fa-cube'];
 
 
 /*
